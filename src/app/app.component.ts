9220a0410dae6cb78de7d871b439a6c4cce7160f
lagmanzaza <lagmanmc@gmail.com>
2021-06-07 23:30:14 +0700

    feat: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { IPost } from './@types/IPost';
+
+const apolloQuery = gql`
+  query {
+    allPosts {
+      id
+      title
+      views
+    }
+  }
+`;
+
+const apolloMutate = gql`
+  mutation create($title: String!, $views: Int!) {
+    createPost(title: $title, views: $views) {
+      id
+      title
+      views
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'china-angular';
+export class AppComponent implements OnInit {
+  title: string = '';
+  views: number = 0;
+  listPosts: IPost[] = [];
+
+  constructor(private apollo: Apollo) {
+    const a: any = {};
+    a['list'] = [];
+    a['list'].push('aaaaaaa');
+    a['list'].push('bbbbbbb');
+    console.log('[a data]', a);
+
+    // KNOW ABOUT MUTABLE DATA
+    // const b: any = Object.freeze({});
+    // b['list'] = [];
+    // b['list'].push('bbbbb');
+  }
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery<{ allPosts: IPost[] }>({ query: apolloQuery })
+      .valueChanges.subscribe(({ data }) => {
+        console.log('[query]', data.allPosts);
+        this.listPosts = data.allPosts;
+      });
+  }
+
+  handleAddNewPost() {
+    this.apollo
+      .mutate<{ createPost: IPost }>({
+        mutation: apolloMutate,
+        variables: {
+          title: this.title,
+          views: this.views,
+        },
+      })
+      .subscribe(({ data }) => {
+        console.log('[mutation]', data?.createPost);
+        this.listPosts = [...this.listPosts, data?.createPost as IPost];
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:3000/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
